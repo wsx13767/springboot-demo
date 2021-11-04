a13e690d96c4d0f3e5505bab1b9c7fe9472b3195
Siang <wsx13767@gmail.com>
2021-11-04 11:53:49 +0800

    gateway
    gateway

---
+++ server-gateway/src/main/java/com/siang/server/gateway/ServerGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.siang.server.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGatewayApplication.class, args);
+	}
+
+}
