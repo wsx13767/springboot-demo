c442afdd09b251f3bed4d73cc05560d1fada07e7
Siang <wsx13767@gmail.com>
2022-03-17 15:57:05 +0800

    新增 config server

--- demo/todo/src/main/java/com/example/sion/todo/TodoApplication.java
+++ demo/todo/src/main/java/com/example/sion/todo/TodoApplication.java
@@ -2,7 +2,9 @@ package com.example.sion.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TodoApplication {
     public static void main(String[] args) {
--- demo/todo/src/main/java/com/example/sion/todo/controller/todo/TodoV2Controller.java
+++ demo/todo/src/main/java/com/example/sion/todo/controller/todo/TodoV2Controller.java
@@ -2,6 +2,7 @@ package com.example.sion.todo.controller.todo;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
